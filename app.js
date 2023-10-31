219d0364c6303e1600f60b84afd9971433d91e88
S565730 <S565730@nwmissouri.edu>
2023-10-31 17:15:36 -0500

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var jaguarRouter = require('./routes/jaguar');
 var boardRouter = require('./routes/board');
+var chooseRouter = require('./routes/choose');
 
 
 var app = express();
@@ -26,6 +27,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/jaguar', jaguarRouter);
 app.use('/board', boardRouter);
+app.use('/choose', chooseRouter);
 
 
 // catch 404 and forward to error handler
